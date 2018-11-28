095873a69ed0464f55afba6622b19ba4ce31999b
lunalotussa <171111047@mhs.stiki.ac.id>
2018-11-28 13:21:44 +0700

    Add files via upload

---
+++ db_penggajian.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 28 Nov 2018 pada 04.03
+-- Versi server: 10.1.31-MariaDB
+-- Versi PHP: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_penggajian`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_golongan`
+--
+
+CREATE TABLE `tb_golongan` (
+  `id_golongan` varchar(20) NOT NULL,
+  `gaji_pokok` int(11) NOT NULL,
+  `transport` int(11) NOT NULL,
+  `makan` int(11) NOT NULL,
+  `lembur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_karyawan`
+--
+
+CREATE TABLE `tb_karyawan` (
+  `id_karyawan` varchar(20) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_tlp` int(11) NOT NULL,
+  `jk` char(2) NOT NULL,
+  `id_golongan` varchar(20) NOT NULL,
+  `id_status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_status`
+--
+
+CREATE TABLE `tb_status` (
+  `id_status` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `tunjangan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_trans`
+--
+
+CREATE TABLE `tb_trans` (
+  `id_trans` varchar(20) NOT NULL,
+  `absen` int(11) NOT NULL,
+  `lembur` int(11) NOT NULL,
+  `id_karyawan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_golongan`
+--
+ALTER TABLE `tb_golongan`
+  ADD PRIMARY KEY (`id_golongan`);
+
+--
+-- Indeks untuk tabel `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indeks untuk tabel `tb_status`
+--
+ALTER TABLE `tb_status`
+  ADD PRIMARY KEY (`id_status`);
+
+--
+-- Indeks untuk tabel `tb_trans`
+--
+ALTER TABLE `tb_trans`
+  ADD PRIMARY KEY (`id_trans`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
